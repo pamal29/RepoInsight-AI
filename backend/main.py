4cea391d6d7ba1c5670c1c50d8818db216ad10ea
pamal pahasara <pamalpahasara@gmail.com>
2026-02-27 09:55:14 +0530

    FastAPI Entry Point with main.py

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from github.fetcher import get_repo_contents
+from utils.file_filter import filter_files
+from analyzers.language_detector import detect_language
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+  return {"message": "RepoInsight AI is running"}
+
+@app.post("/analyze")
+def analyze_repo(repo_url: str):
+  files = get_repo_contents(repo_url)
+  filtered_files = filter_files(files)
+  language_info = detect_language(filtered_files)
+
+  return{
+    "total_files": len(filtered_files),
+    "language_info": language_info
+  }
+
+  
