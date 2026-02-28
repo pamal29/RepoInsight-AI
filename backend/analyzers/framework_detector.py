a9e1a2851041f1d42c7367a0d05fc5c4101e85ff
pamal pahasara <pamalpahasara@gmail.com>
2026-02-28 08:31:11 +0530

    framework detection

---
+++ backend/analyzers/framework_detector.py
@@ -0,0 +1,29 @@
+import os
+
+def detect_framework(file_path):
+  framework = []
+
+  for root, dirs, files in os.walk(file_path):
+    for file in files:
+      file_path = os.path.join(root, file)
+
+      if file == "requirements.txt":
+        with open(file_path, "r", encoding="utf-8") as f:
+          content = f.read().lower()
+          if "flask" in content:
+            framework.append("Flask")
+
+      if file == "package.json":
+        with open(file_path, "r", encoding="utf-8") as f:
+          content = f.read().lower()
+          if "react" in content:
+            framework.append("React")
+
+      if file == "pom.xml":
+        with open(file_path, "r", encoding="utf-8") as f:
+          content = f.read().lower()
+          if "spring" in content:
+            framework.append("Spring")
+
+    
+return list(set(framework))
