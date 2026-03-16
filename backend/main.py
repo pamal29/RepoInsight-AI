34c97c9d3a31ea5f68c81e7de270f532169ceef9
pamal pahasara <pamalpahasara@gmail.com>
2026-03-16 14:16:18 +0530

    add proper httpexception error responces

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,11 @@ from analyzers.architecture_detector import detect_architecture
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
+app = FastAPI(
+  title="RepoInsight AI",
+  description = "Analyze your GitHub repositories with AI",
+  version="1.0.0"
+)
   
 app.add_middleware(
     CORSMiddleware,
@@ -18,29 +22,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class AnalyzeRequest(BaseModel):
+  repo_url:str
+
 @app.get("/")
 def root():
   return {"message": "RepoInsight AI is running"}
 
 @app.post("/analyze")
-def analyze_repo(repo_url: str):
+def analyze_repo(request : AnalyzeRequest):
+  try:
 
-  files = get_repo_contents(repo_url)
+    files = get_repo_contents(request.repo_url)
+  except Exception as e:
+    raise HTTPException(status_code=400, detail="Error fetching repository contents")
+
+  if not files:
+    raise HTTPException(status_code=400, detail="No files to analyze")
 
   filtered_files = filter_files(files)
-  
+
+  if not filtered_files:
+    raise HTTPException(status_code=400, detail="No analyzable files found after filtering")
+
   language_info = detect_language(filtered_files)
-    
   frameworks = detect_framework(filtered_files)
   complexity = calculate_complexity(filtered_files)
   architecture = detect_architecture(filtered_files)
 
+
   return {
-      "total_files": len(filtered_files),
-      "language_info": language_info,
-      "frameworks": frameworks,
-      "complexity": complexity,
-      "architecture": architecture
+       "repo_url": request.repo_url,
+        "total_files": len(filtered_files),
+        "language_info": language_info,
+        "frameworks": frameworks,
+        "complexity": complexity,
+        "architecture": architecture,
   }
 
 
