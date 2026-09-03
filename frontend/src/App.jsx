2ed463dfb3aaab3402776b9e0b4027ed3e2faddc
pamal pahasara <pamalpahasara@gmail.com>
2026-09-03 17:47:40 +0530

    change port that fronent send req to

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_BASE = "http://127.0.0.1:8080";
+const API_BASE = "http://127.0.0.1:8000";
 
 export default function App() {
   const [repoUrl, setRepoUrl] = useState("");
